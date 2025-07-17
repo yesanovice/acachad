26a0f0607cb20da8a617c0fc1cbbb09829377b8a
yesanovice <arkokazi08@gmail.com>
2025-07-17 22:52:37 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,35 @@
-const CACHE_NAME = 'academic-progress-tracker-v1';
-const ASSETS_TO_CACHE = [
+const CACHE_NAME = 'progress-tracker-cache-v1';
+const urlsToCache = [
   '/',
-  '/index.html',
+  '/index.htm',
+  '/styles.css',
+  '/app.js',
   '/manifest.json',
-  // Add other assets you want to cache here
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(ASSETS_TO_CACHE);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
       );
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
